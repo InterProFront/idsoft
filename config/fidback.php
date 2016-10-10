fc817a531fcc6863d847398181051eb76b25de1d
InterProFront <fallzem@gmail.com>
2016-10-10 11:30:01 +0600

    fix mail

--- config/fidback.php
+++ config/fidback.php
@@ -2,9 +2,9 @@
 
 return [
     'block_name' => 'fidback',
-    'mail_username' => 'sales@idsoft.info',
+    'mail_username' => 'demigod96@bk.ru',
     'site_name' => 'IDsoft.kz',
-    'mail_rec' => 'demigod96@bk.ru',
+    'mail_rec' => 'sales@idsoft.info',
     'mail_rec_copy1' => 'demigod96@bk.ru',
     'mail_rec_copy2' => 'demigod96@bk.ru',
     'mail_rec_copy3' => 'demigod96@bk.ru',
