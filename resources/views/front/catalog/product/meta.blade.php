59584e8d01abec5c9bdb3859dc80a55f29b7cfe1
Konstantin <mr.kaluchii@mail.ru>
2016-11-01 19:54:03 +0600

    Rubbish

--- resources/views/front/catalog/product/meta.blade.php
+++ resources/views/front/catalog/product/meta.blade.php
@@ -1,5 +1,5 @@
 @section('meta')
-    <title>{{$product->page_title_field}}</title>
-    <meta name="description" content="{{$product->seo_description_field}}">
-    <meta name="keywords" content="{{$product->seo_keywords_field}}">
+    <title>{{$product->product_name_field}} купить в Алматы, Казахстане с доставкой - ID SOFT</title>
+    <meta name="description" content="Интернет магазин торгового оборудования ID SOFT предлагает заказать {{$product->product_name_field}} недорого. Лучшая цена, доставка, отзывы. ☎ +7 (727) 220-2000">
+    <meta name="keywords" content="{{$product->product_name_field}}, {{$product->product_name_field}} купить, {{$product->product_name_field}} заказать">
 @endsection
