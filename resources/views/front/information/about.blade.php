3e24050c03300e5a685f7137440d3518e99f95bd
Kaluchii <mr.kaluchii@mail.ru>
2016-11-08 20:20:18 +0600

    len

--- resources/views/front/information/about.blade.php
+++ resources/views/front/information/about.blade.php
@@ -40,9 +40,14 @@
                     <div class="col-1-2">
                         <div class="name">Ольга Ли</div>
                         <div class="position">Главный бухгалтер</div>
-                        <div class="about-member">Профессиональный бухгалтер РК, член ассоциации профессиональных бухгалтеров и бухгалтерских организаций.
+                        <div class="text-block"><p>Профессиональный бухгалтер РК, член ассоциации профессиональных
+                                бухгалтеров и бухгалтерских организаций.
 
-                            Практический опыт работы — более 15 лет. Два высших образования (техническое и экономическое). В разное время работала главным бухгалтером в крупных частных и государственных компаниях. Является практикующим консультантом по финансовым вопросам и бухгалтерскому учету.</div>
+                                Практический опыт работы — более 15 лет. Два высших образования (техническое и
+                                экономическое). В разное время работала главным бухгалтером в крупных частных и
+                                государственных компаниях. Является практикующим консультантом по финансовым вопросам и
+                                бухгалтерскому учету.</div>
+                        </p>
                     </div>
                 </div>
                 <div class="staff-item">
@@ -52,9 +57,14 @@
                     <div class="col-1-2">
                         <div class="name">Ольга Кобзева</div>
                         <div class="position">Директор</div>
-                        <div class="about-member">Профессиональный бухгалтер РК, член ассоциации профессиональных бухгалтеров и бухгалтерских организаций.
+                        <div class="text-block"><p>Профессиональный бухгалтер РК, член ассоциации профессиональных
+                                бухгалтеров и бухгалтерских организаций.
 
-                            Специалист по налоговому учету. Высшее экономическое образование. Общий стаж работы — более 13 лет, 10 из которых в аутсорсинге. Имеет практический опыт работы с Казахстанскими и зарубежными организациями, работающие в области малого и среднего бизнеса, а так же некоммерческие организации.</div>
+                                Специалист по налоговому учету. Высшее экономическое образование. Общий стаж работы —
+                                более 13 лет, 10 из которых в аутсорсинге. Имеет практический опыт работы с
+                                Казахстанскими и зарубежными организациями, работающие в области малого и среднего
+                                бизнеса, а так же некоммерческие организации.</div>
+                        </p>
                     </div>
                 </div>
             </div>
@@ -85,29 +95,76 @@
                         </li>
                     </ul>
                 </div>
-            <span class="totalvotes" itemprop="aggregateRating" itemscope="itemscope"
-                  itemtype="http://schema.org/AggregateRating">
+                <span class="totalvotes" itemprop="aggregateRating" itemscope="itemscope"
+                      itemtype="http://schema.org/AggregateRating">
                     <meta itemprop="ratingValue" content="{{$rating['middle']}}">Текущий рейтинг — {{$rating['middle']}}
-                <meta itemprop="bestRating" content="5">
+                    <meta itemprop="bestRating" content="5">
                     <meta itemprop="ratingCount" content="{{$rating['count']}}"> ({{$rating['count']}} человек)
                     </span>
             </div>
-            <div class="contact-us">
-                <div class="contacts">
-                    <div class="contacts-title"></div>
-                    <div class="consult-text"></div>
-                    <div class="numbers">
-                        <div class="number">
-                            <div class="telephone"></div>
-                            <div class="under-numer"></div>
-                        </div>
-                        <div class="number">
-                            <div class="telephone"></div>
-                            <div class="under-numer"></div>
+        </div>
+        <div class="contact-us">
+            <div class="contacts">
+                <div class="contacts-title">Свяжитесь с нами</div>
+                <div class="consult-text">В рабочее время, вы можете получить консультацию по автоматизации,
+                    оборудованию или программному обеспечению по телефонам
+                </div>
+                <div class="numbers">
+                    <div class="number">
+                        <div class="telephone">+7 (727) 220-20-00</div>
+                        <div class="under-number"> С 10 до 18:00</div>
+                    </div>
+                    <div class="number">
+                        <div class="telephone">+7 747 220-20-00</div>
+                        <div class="under-number">
+                            <div class="icon-group">
+                                <i class="wpp"></i>
+                                <i class="tel"></i>
+                                <i class="vib"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="map"></div>
+                <a class="ask-me" href="#question">Написать письмо</a>
+            </div>
+            <div class="map-wrap">
+                <div id="map">
+                    {{--<script src="http://maps.api.2gis.ru/2.0/loader.js?pkg=full"></script>
+                    <script type="text/javascript">
+                        var map;
+
+                        DG.then(function () {
+                            map = DG.map('map', {
+                                center: [43.22912475875056, 76.89061760902406],
+                                zoom: 17,
+                                fullscreenControl: false,
+                            });
+
+                            DG.marker([43.22912475875056, 76.89061760902406]).addTo(map).bindPopup('Вы кликнули по мне!');
+                        });
+                    </script>--}}
+                    {{--<a class="dg-widget-link"
+                       href="http://2gis.kz/almaty/firm/9429940001659307/center/76.89061760902406,43.22912475875056/zoom/17?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=bigMap">Посмотреть
+                        на карте Алматы</a>
+                    <div class="dg-widget-link"><a
+                                href="http://2gis.kz/almaty/firm/9429940001659307/photos/9429940001659307/center/76.89061760902406,43.22912475875056/zoom/17?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=photos">Фотографии
+                            компании</a></div>
+                    <div class="dg-widget-link"><a
+                                href="http://2gis.kz/almaty/center/76.890395,43.22905/zoom/17/routeTab/rsType/bus/to/76.890395,43.22905╎Центр автоматизации торговли IDSOFT, ТОО, компания?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=route">Найти
+                            проезд до Центр автоматизации торговли IDSOFT, ТОО, компания</a></div>
+                    <script charset="utf-8" src="http://widgets.2gis.com/js/DGWidgetLoader.js"></script>
+                    <script charset="utf-8">new DGWidgetLoader({
+                            "width": 500,
+                            "height": 400,
+                            "borderColor": "#a3a3a3",
+                            "pos": {"lat": 43.22912475875056, "lon": 76.89061760902406, "zoom": 17},
+                            "opt": {"city": "almaty"},
+                            "org": [{"id": "9429940001659307"}]
+                        });</script>
+                    <noscript style="color:#c00;font-size:16px;font-weight:bold;">Виджет карты использует JavaScript.
+                        Включите его в настройках вашего браузера.
+                    </noscript>--}}
+                </div>
             </div>
         </div>
     </section>
