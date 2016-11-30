260444915cc495b87dc2702d22c83d48ddeeada7
Kaluchii <mr.kaluchii@mail.ru>
2016-11-30 15:08:54 +0600

    Seo tags

--- resources/views/front/catalog/product/meta.blade.php
+++ resources/views/front/catalog/product/meta.blade.php
@@ -1,5 +1,19 @@
 @section('meta')
-    <title>{{$product->product_name_field}} купить в Алматы, Казахстане с доставкой - IDSOFT</title>
-    <meta name="description" content="Интернет магазин торгового оборудования ID SOFT предлагает заказать {{$product->product_name_field}} недорого. Лучшая цена, доставка, отзывы. ☎ +7 (727) 220-2020">
-    <meta name="keywords" content="{{$product->product_name_field}}, {{$product->product_name_field}} купить, {{$product->product_name_field}} заказать">
+    @if($product->page_title_field == '')
+        <title>{{$product->product_name_field}} купить в Алматы, Казахстане с доставкой - IDSOFT</title>
+    @else
+        <title>{{$product->page_title_field}}</title>
+    @endif
+
+    @if($product->seo_description_field == '')
+        <meta name="description" content="Интернет магазин торгового оборудования IDSOFT предлагает заказать {{$product->product_name_field}} недорого. Лучшая цена, доставка, отзывы. ☎ +7 (727) 220-2020">
+    @else
+        <meta name="description" content="{{$product->seo_description_field}}">
+    @endif
+
+    @if($product->seo_keywords_field == '')
+        <meta name="keywords" content="{{$product->product_name_field}}, {{$product->product_name_field}} купить, заказать {{$product->product_name_field}}">
+    @else
+        <meta name="keywords" content="{{$product->seo_keywords_field}}">
+    @endif
 @endsection
