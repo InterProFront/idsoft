b85330442e60417d4d556b1ba88548500e5fab05
kaluchii <mr.kaluchii@mail.ru>
2017-08-10 10:28:14 +0600

    Add facebook pixel

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -14,6 +14,21 @@
     @yield('styles')
     @yield('meta')
     <script charset="UTF-8" src="//cdn.sendpulse.com/28edd3380a1c17cf65b137fe96516659/js/push/5cdef2d298991ff1822dcdcd3a3a5384_1.js" async></script>
+    <!— Facebook Pixel Code —>
+    <script>
+        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+            document,'script','https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '257698391410875'); // Insert your pixel ID here.
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=257698391410875&ev=PageView&noscript=1"
+        /></noscript>
+    <!— DO NOT MODIFY —>
+    <!— End Facebook Pixel Code —>
 </head>
 <body>
 <p class="top">{{$static->logo_text_field}}</p>
